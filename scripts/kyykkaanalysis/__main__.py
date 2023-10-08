2bee8f304936f5d2b82f034d8238b24fc0ff8e2b
Elias Räsänen <rasanen.elias@gmail.com>
2023-10-08 11:39:41 +0300

    Parsing arguments

--- scripts/kyykkaanalysis/__main__.py
+++ scripts/kyykkaanalysis/__main__.py
@@ -1,5 +1,29 @@
 """Analysis of kyykka play times"""
+from pathlib import Path
+from argparse import ArgumentParser, Namespace
+
 
 def main():
-    """Run analysis"""
-    pass
+    """
+    Run analysis
+    """
+
+    args = _parse_arguments()
+
+
+def _parse_arguments() -> Namespace:
+    parser = ArgumentParser()
+
+    parser.add_argument(
+        "input_file",
+        help="Path to the CSV file which contains the input data",
+        type=Path,
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+
+if __name__ == "__main__":
+    main()
