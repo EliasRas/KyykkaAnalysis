abd407469a9a41f2ec121503f32fb38440601b2a
Elias Räsänen <rasanen.elias@gmail.com>
2023-12-09 10:05:03 +0200

    Fix docstring

--- scripts/kyykkaanalysis/model/model_checks.py
+++ scripts/kyykkaanalysis/model/model_checks.py
@@ -9,6 +9,13 @@ from ..figures.prior import parameter_distributions
 def check_priors(data: list[Stream], figure_directory: Path):
     """
     Sample data from prior distribution and analyze it
+
+    Parameters
+    ----------
+    data : list of Stream
+        Throw time data
+    figure_directory : Path
+        Path to the directory in which the figures are saved
     """
 
     model = ThrowTimeModel(ModelData(data))
