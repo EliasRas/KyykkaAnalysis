3b83efbad2021a59747e646b82bae82e9ec06d9e
Elias Räsänen <rasanen.elias@gmail.com>
2024-03-31 14:31:53 +0300

    Calling analysis

--- scripts/kyykkaanalysis/__main__.py
+++ scripts/kyykkaanalysis/__main__.py
@@ -1,8 +1,12 @@
 """Analysis of kyykka play times"""
+
 from pathlib import Path
 from argparse import ArgumentParser, Namespace
 
 from kyykkaanalysis.data.data_reading import read_times
+from kyykkaanalysis.data.data_description import print_description
+from kyykkaanalysis.figures import data as data_figures
+from kyykkaanalysis.model import model_checks
 
 
 def main():
@@ -12,6 +16,14 @@ def main():
 
     args = _parse_arguments()
     data = read_times(args.input_file)
+
+    print_description(data)
+
+    data_figures.time_distributions(data, args.figure_directory)
+    data_figures.averages(data, args.figure_directory)
+
+    model_checks.check_priors(data, args.figure_directory, args.cache_directory)
+    model_checks.fake_data_simulation(data, args.figure_directory, args.cache_directory)
 
 
 def _parse_arguments() -> Namespace:
