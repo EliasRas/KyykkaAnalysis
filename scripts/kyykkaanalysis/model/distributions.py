66c4441227d94cd35b52641a1c2b33432b25ad52
Elias Räsänen <rasanen.elias@gmail.com>
2024-04-17 16:21:00 +0300

    Option to choose between fast and numerically stable implementations. Docstring improvements

--- scripts/kyykkaanalysis/model/distributions.py
+++ scripts/kyykkaanalysis/model/distributions.py
@@ -5,7 +5,7 @@ This module provides implementations for functions that define distributions (e.
 densities and random number generators).
 """
 
-from collections.abc import Sequence
+from collections.abc import Callable, Sequence
 
 import numpy as np
 from numpy import typing as npt
@@ -17,9 +17,45 @@ from pytensor.tensor.variable import TensorVariable
 DATA_INPUT_TYPE = (
     float | npt.NDArray[np.float64] | TensorVariable | Sequence[TensorVariable]
 )
+"""The allowed data types for the value argument of likelihood functions."""
 
 
 def podium_gamma_logp(
+    extra_stability: bool,  # noqa: FBT001
+) -> Callable[
+    [DATA_INPUT_TYPE, float | TensorVariable, float | TensorVariable], TensorVariable
+]:
+    """
+    Return an implementation of gamma likelihood with podium measurement error.
+
+    Returns a function which calculates the likelihood a gamma distributed random sample
+    measured with podium error. Podium error is an error distribution for two random
+    variables measured with independent uniform error between [-1, 1] and rounded down.
+
+    Parameters
+    ----------
+    extra_stability : bool
+        Whether the implementation is numerically more stable
+
+    Returns
+    -------
+    function with signature (
+        DATA_INPUT_TYPE,
+        float | pytensor.tensor.variable.TensorVariable,
+        float | pytensor.tensor.variable.TensorVariable
+        ) -> pytensor.tensor.variable.TensorVariable
+        Likelihood function
+
+    See Also
+    --------
+    The error model and the motivation for it is explained more thoroughly in the
+    report stored in KyykkaAnalysis repo.
+    """
+
+    return _stable_podium_gamma_logp if extra_stability else _podium_gamma_logp
+
+
+def _podium_gamma_logp(
     value: DATA_INPUT_TYPE,
     k: float | TensorVariable,
     theta: float | TensorVariable,
@@ -35,14 +71,61 @@ def podium_gamma_logp(
     ----------
     value : DATA_INPUT_TYPE
         Value of random variables
-    k : float or TensorVariable
+    k : float or pytensor.tensor.variable.TensorVariable
         Shape parameter of gamma distribution
-    theta : float or TensorVariable
+    theta : float or pytensor.tensor.variable.TensorVariable
         Mean of gamma distribution
 
     Returns
     -------
-    TensorVariable
+    pytensor.tensor.variable.TensorVariable
+        Log density
+
+    See Also
+    --------
+    The error model and the motivation for it is explained more thoroughly in the
+    report stored in KyykkaAnalysis repo.
+    """
+
+    alpha = k
+    beta = k / theta
+
+    value = value + expand_dims([-2, -1, 0, 1, 2, 3], -1)
+    densities = _gammainc(alpha, beta * value)
+
+    # Differences of gamma distribution's CDF
+    densities = densities[3:, :][::-1, :] - densities[:3, :]
+    weights = expand_dims([5 / 9, 3 / 9, 1 / 9], -1)
+
+    return log(sum(densities * weights, 0))
+
+
+def _stable_podium_gamma_logp(
+    value: DATA_INPUT_TYPE,
+    k: float | TensorVariable,
+    theta: float | TensorVariable,
+) -> TensorVariable:
+    """
+    Log density of gamma distributed random variable with podium measurement error.
+
+    Calculates the log probability density of a gamma distributed random variable that
+    was measured with podium error. Podium error is an error distribution for two random
+    variables measured with independent uniform error between [-1, 1] and rounded down.
+
+    This implementation is numerically more stable for large values
+
+    Parameters
+    ----------
+    value : DATA_INPUT_TYPE
+        Value of random variables
+    k : float or pytensor.tensor.variable.TensorVariable
+        Shape parameter of gamma distribution
+    theta : float or pytensor.tensor.variable.TensorVariable
+        Mean of gamma distribution
+
+    Returns
+    -------
+    pytensor.tensor.variable.TensorVariable
         Log density
 
     See Also
@@ -127,6 +210,35 @@ def podium_gamma_rng(
 
 
 def floor_gamma_logp(
+    extra_stability: bool,  # noqa: FBT001
+) -> Callable[
+    [DATA_INPUT_TYPE, float | TensorVariable, float | TensorVariable], TensorVariable
+]:
+    """
+    Return an implementation of gamma likelihood with floor rounding measurement error.
+
+    Returns a function which calculates the likelihood a gamma distributed random sample
+    measured with floor rounding error.
+
+    Parameters
+    ----------
+    extra_stability : bool
+        Whether the implementation is numerically more stable
+
+    Returns
+    -------
+    function with signature (
+        DATA_INPUT_TYPE,
+        float | pytensor.tensor.variable.TensorVariable,
+        float | pytensor.tensor.variable.TensorVariable
+        ) -> pytensor.tensor.variable.TensorVariable
+        Likelihood function
+    """
+
+    return _stable_floor_gamma_logp if extra_stability else _floor_gamma_logp
+
+
+def _floor_gamma_logp(
     value: DATA_INPUT_TYPE,
     k: float | TensorVariable,
     theta: float | TensorVariable,
@@ -141,14 +253,52 @@ def floor_gamma_logp(
     ----------
     value : DATA_INPUT_TYPE
         Value of random variables
-    k : float or TensorVariable
+    k : float or pytensor.tensor.variable.TensorVariable
         Shape parameter of gamma distribution
-    theta : float or TensorVariable
+    theta : float or pytensor.tensor.variable.TensorVariable
         Mean of gamma distribution
 
     Returns
     -------
-    TensorVariable
+    pytensor.tensor.variable.TensorVariable
+        Log density
+    """
+
+    alpha = k
+    beta = k / theta
+
+    value = value + expand_dims([0, 1], -1)
+    densities = _gammainc(alpha, beta * value)
+
+    # Differences of gamma distribution's CDF
+    densities = densities[1, :] - densities[0, :]
+
+    return log(densities)
+
+
+def _stable_floor_gamma_logp(
+    value: DATA_INPUT_TYPE,
+    k: float | TensorVariable,
+    theta: float | TensorVariable,
+) -> TensorVariable:
+    """
+    Log density of gamma distributed random variable with floor rounding error.
+
+    Calculates the log probability density of a gamma distributed random variable that
+    was measured with floor rounding error.
+
+    Parameters
+    ----------
+    value : DATA_INPUT_TYPE
+        Value of random variables
+    k : float or pytensor.tensor.variable.TensorVariable
+        Shape parameter of gamma distribution
+    theta : float or pytensor.tensor.variable.TensorVariable
+        Mean of gamma distribution
+
+    Returns
+    -------
+    pytensor.tensor.variable.TensorVariable
         Log density
     """
 
@@ -215,6 +365,42 @@ def floor_gamma_rng(
 
 
 def podium_invgamma_logp(
+    extra_stability: bool,  # noqa: FBT001
+) -> Callable[
+    [DATA_INPUT_TYPE, float | TensorVariable, float | TensorVariable], TensorVariable
+]:
+    """
+    Return an implementation of inverse gamma likelihood with podium measurement error.
+
+    Returns a function which calculates the likelihood an inverse gamma distributed
+    random sample measured with podium error. Podium error is an error distribution
+    for two random variables measured with independent uniform error between [-1, 1] and
+    rounded down.
+
+    Parameters
+    ----------
+    extra_stability : bool
+        Whether the implementation is numerically more stable
+
+    Returns
+    -------
+    function with signature (
+        DATA_INPUT_TYPE,
+        float | pytensor.tensor.variable.TensorVariable,
+        float | pytensor.tensor.variable.TensorVariable
+        ) -> pytensor.tensor.variable.TensorVariable
+        Likelihood function
+
+    See Also
+    --------
+    The error model and the motivation for it is explained more thoroughly in the
+    report stored in KyykkaAnalysis repo.
+    """
+
+    return _stable_podium_invgamma_logp if extra_stability else _podium_invgamma_logp
+
+
+def _podium_invgamma_logp(
     value: DATA_INPUT_TYPE,
     a: float | TensorVariable,
     theta: float | TensorVariable,
@@ -231,15 +417,64 @@ def podium_invgamma_logp(
     ----------
     value : DATA_INPUT_TYPE
         Value of random variables
-    a : float or TensorVariable
+    a : float or pytensor.tensor.variable.TensorVariable
         Transformed shape parameter of inverse gamma distribution. Shape parameter
         alpha = exp(-a) + 1
-    theta : float or TensorVariable
+    theta : float or pytensor.tensor.variable.TensorVariable
         Mean of inverse gamma distribution
 
     Returns
     -------
-    TensorVariable
+    pytensor.tensor.variable.TensorVariable
+        Log density
+
+    See Also
+    --------
+    The error model and the motivation for it is explained more thoroughly in the
+    report stored in KyykkaAnalysis repo.
+    """
+
+    alpha = exp(-a) + 1
+    beta = theta * exp(-a)
+
+    value = value + expand_dims([-2, -1, 0, 1, 2, 3], -1)
+    densities = (_gammaincc(alpha, beta / value),)
+
+    # Differences of inverse gamma distribution's CDF
+    densities = densities[3:, :][::-1, :] - densities[:3, :]
+    weights = expand_dims([5 / 9, 3 / 9, 1 / 9], -1)
+
+    return log(sum(densities * weights, 0))
+
+
+def _stable_podium_invgamma_logp(
+    value: DATA_INPUT_TYPE,
+    a: float | TensorVariable,
+    theta: float | TensorVariable,
+) -> TensorVariable:
+    """
+    Log density of inverse gamma distributed random variable with podium error.
+
+    Calculates the log probability density of an inverse gamma distributed random
+    variable that was measured with podium error. Podium error is an error distribution
+    for two random variables measured with independent uniform error between [-1, 1] and
+    rounded down.
+
+    This implementation is numerically more stable for large values
+
+    Parameters
+    ----------
+    value : DATA_INPUT_TYPE
+        Value of random variables
+    a : float or pytensor.tensor.variable.TensorVariable
+        Transformed shape parameter of inverse gamma distribution. Shape parameter
+        alpha = exp(-a) + 1
+    theta : float or pytensor.tensor.variable.TensorVariable
+        Mean of inverse gamma distribution
+
+    Returns
+    -------
+    pytensor.tensor.variable.TensorVariable
         Log density
 
     See Also
@@ -318,6 +553,35 @@ def podium_invgamma_rng(
 
 
 def floor_invgamma_logp(
+    extra_stability: bool,  # noqa: FBT001
+) -> Callable[
+    [DATA_INPUT_TYPE, float | TensorVariable, float | TensorVariable], TensorVariable
+]:
+    """
+    Return an implementation of inverse gamma likelihood with floor rounding error.
+
+    Returns a function which calculates the likelihood a gamma distributed random sample
+    measured with floor rounding error.
+
+    Parameters
+    ----------
+    extra_stability : bool
+        Whether the implementation is numerically more stable
+
+    Returns
+    -------
+    function with signature (
+        DATA_INPUT_TYPE,
+        float | pytensor.tensor.variable.TensorVariable,
+        float | pytensor.tensor.variable.TensorVariable
+        ) -> pytensor.tensor.variable.TensorVariable
+        Likelihood function
+    """
+
+    return _stable_floor_invgamma_logp if extra_stability else _floor_invgamma_logp
+
+
+def _floor_invgamma_logp(
     value: DATA_INPUT_TYPE,
     a: float | TensorVariable,
     theta: float | TensorVariable,
@@ -332,15 +596,54 @@ def floor_invgamma_logp(
     ----------
     value : DATA_INPUT_TYPE
         Value of random variables
-    a : float or TensorVariable
+    a : float or pytensor.tensor.variable.TensorVariable
         Transformed shape parameter of inverse gamma distribution. Shape parameter
         alpha = exp(-a) + 1
-    theta : float or TensorVariable
+    theta : float or pytensor.tensor.variable.TensorVariable
         Mean of inverse gamma distribution
 
     Returns
     -------
-    TensorVariable
+    pytensor.tensor.variable.TensorVariable
+        Log density
+    """
+
+    alpha = exp(-a) + 1
+    beta = theta * exp(-a)
+
+    value = value + expand_dims([0, 1], -1)
+    densities = _gammaincc(alpha, beta / value)
+
+    # Difference of inverse gamma distribution's CDF
+    densities = densities[1, :] - densities[0, :]
+
+    return log(densities)
+
+
+def _stable_floor_invgamma_logp(
+    value: DATA_INPUT_TYPE,
+    a: float | TensorVariable,
+    theta: float | TensorVariable,
+) -> TensorVariable:
+    """
+    Log density of inverse gamma distributed random variable with floor rounding error.
+
+    Calculates the log probability density of an inverse gamma distributed random
+    variable that was measured with floor rounding error.
+
+    Parameters
+    ----------
+    value : DATA_INPUT_TYPE
+        Value of random variables
+    a : float or pytensor.tensor.variable.TensorVariable
+        Transformed shape parameter of inverse gamma distribution. Shape parameter
+        alpha = exp(-a) + 1
+    theta : float or pytensor.tensor.variable.TensorVariable
+        Mean of inverse gamma distribution
+
+    Returns
+    -------
+    pytensor.tensor.variable.TensorVariable
         Log density
     """
 
